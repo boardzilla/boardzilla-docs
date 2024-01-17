a0a635065d8064393cd43470b69a2ebc62a724c3
Joshua Hull <josh@fireflop.com>
2024-01-17 16:19:08 +0100

    Switch to warn

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   organizationName: "boardzilla",
   trailingSlash: false,
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "throw",
 
   // Even if you don't use internalization, you can use this field to set useful
